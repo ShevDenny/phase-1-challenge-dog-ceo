f1b24798c1c06b6fc01d64904e3b0e081a18b315
zeus <zeus.ramirez347@gmail.com>
2021-05-25 16:46:31 -0400

    Challenge 2 completed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
 
-
-function getUrls(imgUrl){
+function getImgUrls(imgUrl){
     fetch(imgUrl)
     .then(res =>res.json())
     .then(data => data.message.forEach(renderImg))
 }
-
-
 
 const renderImg = function(data){
     let img = document.createElement('img')
     img.src = data
     document.querySelector('#dog-image-container').append(img)
 }
-getUrls(imgUrl)
+getImgUrls(imgUrl)
+
+function getBreedUrls(breedUrl){
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then(data => Object.keys(data.message).forEach(renderBreed))
+}
+
+const renderBreed = function(data){
+    let li = document.createElement('li')
+    li.textContent = data
+    document.querySelector('#dog-breeds').append(li)
+}
+getBreedUrls(breedUrl)
