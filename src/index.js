e0a359f7b3ace6258a555ef116c5842879296034
ShevDenny <sdenny904@gmail.com>
2021-05-25 14:48:49 -0400

    work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl)
+.then(res =>res.json())
+.then(data =>(data.forEach(element => document.createElement('img'))))
+
