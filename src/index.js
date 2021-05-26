07410d5d4a6b36beaacbcca0ace7f6463b9b95ac
ShevDenny <sdenny904@gmail.com>
2021-05-26 10:17:14 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+document.addEventListener('DOMContentLoaded', () => {
 
 function getImgUrls(imgUrl){
     fetch(imgUrl)
@@ -25,5 +26,29 @@ const renderBreed = function(data){
     let li = document.createElement('li')
     li.textContent = data
     document.querySelector('#dog-breeds').append(li)
+
+    li.addEventListener('click', () => {
+        li.style.color= "red"
+    })
+
+
 }
 getBreedUrls(breedUrl)
+
+let dogBreed = document.querySelectorAll('option')
+dogBreed.addEventListener('click', (e) => {
+    //let startLetter= e.target.value;
+    //document.querySelector('#dog-breeds').remove(li)
+    console.log('test')
+
+})
+})
+
+let dogList = document.querySelector('dog-breeds')
+dogList.filter(()=> {
+    let dropDown= document.getElementById('breed-dropdown');
+
+    dropDown.appendChild(dropDownlist)
+
+    
+})
